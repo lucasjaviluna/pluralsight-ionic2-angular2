5234edd91521c0debaa3f43483e1fb6ede887d2f
Lucas <Lucas>
2017-04-18 14:53:50 -0300

    Side menu navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 // import { HomePage } from '../pages/home/home';
 // import { ListPage } from '../pages/list/list';
 
-import {MyTeamsPage} from '../pages/pages';
+import {MyTeamsPage, Tournaments} from '../pages/pages';
 
 //import {MyTeamsPage} from '../pages/my-teams/my-teams.page';
 
@@ -41,9 +41,16 @@ export class MyApp {
     });
   }
 
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+  goHome() {
+    this.nav.push(MyTeamsPage);
   }
+
+  goToTournaments() {
+    this.nav.push(Tournaments);
+  }
+  // openPage(page) {
+  //   // Reset the content nav to have just this page
+  //   // we wouldn't want the back button to show in this scenario
+  //   this.nav.setRoot(page.component);
+  // }
 }
