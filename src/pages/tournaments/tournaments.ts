21675feb68221f4e8cbb0e54f30584037ba01481
Lucas <Lucas>
2017-04-19 12:58:23 -0300

    Loading Component with LoadingController

--- src/pages/tournaments/tournaments.ts
+++ src/pages/tournaments/tournaments.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 // import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { NavController, NavParams } from 'ionic-angular';
+import { LoadingController, NavController, NavParams } from 'ionic-angular';
 
 import {MyTeamsPage, Teams} from '../pages';
 
@@ -20,7 +20,10 @@ export class Tournaments {
 
   tournaments: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private eliteApi: EliteApi) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private eliteApi: EliteApi,
+              private loadingController: LoadingController) {
   }
 
   itemTapped(event, tournament) {
@@ -29,12 +32,19 @@ export class Tournaments {
 
   ionViewDidLoad() {
     console.log('lifecycle: ionViewDidLoad Tournaments');
-    this.eliteApi.getTournaments().then(data => this.tournaments = data);
-  }
 
-  ionViewLoaded() {
-    console.log('lifecycle: ionViewLoaded Tournaments');
-    //this.eliteApi.getTournaments().then(data => this.tournaments = data);
+    let loader = this.loadingController.create({
+      content: 'Getting tournaments...',
+      spinner: 'dots'
+    });
+
+    //hacemos la peticion adentro del loader, asi cuando termina sacamos el loader con el dismiss
+    loader.present().then(() => {
+      this.eliteApi.getTournaments().then(data => {
+        this.tournaments = data;
+        loader.dismiss();
+      });
+    });
   }
 
   ionViewWillEnter() {
